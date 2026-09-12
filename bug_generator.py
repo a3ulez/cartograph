3209a2e71fdc953843f8769b9b9309adc9cc6576
Chelsea Sanchez <136256184+a3ulez@users.noreply.github.com>
2026-09-12 09:27:38 -0500

    Create bug_generator.py
    Not functional, just notes

---
+++ bug_generator.py
@@ -0,0 +1,35 @@
+#Program for generation of original “bugs” and arachnids
+#Designed for Cartograph extension Ink and Expedition by S. Wilson
+
+#Imports
+import itertools, random
+import json
+from datetime import datetime
+#imports dialog box option
+import tkinter as tk
+from tkinter import filedialog
+# Prevent an empty Tkinter window from popping up
+root = tk.Tk()
+root.withdraw()
+
+#Classes
+Bug():
+#Initialize New Bug
+
+#Setters and Getters
+
+#Class Functions
+
+#Functions
+
+#Main
+def main():
+  #Variables
+  bug_catalogue = 
+  size = 0
+
+  #Save Bug
+#Add Bug to Catalogue 
+
+init __main__():
+
